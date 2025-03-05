76318a1c12fa3cb6f5d4b041af5a1744ca8f625c
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2025-03-05 23:44:01 +0000

    Update winit

--- code/tutorial2-surface/src/lib.rs
+++ code/tutorial2-surface/src/lib.rs
@@ -1,32 +1,83 @@
-use std::iter;
+use std::{iter, pin::Pin};
 
+use pollster::FutureExt;
 use winit::{
+    application::ApplicationHandler,
     event::*,
-    event_loop::EventLoop,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
-    window::{Window, WindowBuilder},
+    window::{Window, WindowId},
 };
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
 struct State<'a> {
+    window: Pin<Box<Window>>,
     surface: wgpu::Surface<'a>,
     device: wgpu::Device,
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     clear_color: wgpu::Color,
-    // The window must be declared after the surface so
-    // it gets dropped after it as the surface contains
-    // unsafe references to the window's resources.
-    window: &'a Window,
+    surface_configured: bool,
+}
+
+#[derive(Default)]
+struct App<'a> {
+    state: Option<State<'a>>,
+}
+
+impl<'a> ApplicationHandler for App<'a> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.state = Some(
+            State::new(
+                event_loop
+                    .create_window(Window::default_attributes())
+                    .unwrap(),
+            )
+            .block_on(),
+        );
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match &mut self.state {
+            Some(s) => s.window_event(event_loop, window_id, event),
+            _ => {}
+        }
+    }
 }
 
 impl<'a> State<'a> {
-    async fn new(window: &'a Window) -> State<'a> {
-        let size = window.inner_size();
+    async fn new(w: Window) -> Self {
+        #[cfg(target_arch = "wasm32")]
+        {
+            // Winit prevents sizing with CSS, so we have to set
+            // the size manually when on web.
+            use winit::dpi::PhysicalSize;
 
+            use winit::platform::web::WindowExtWebSys;
+            web_sys::window()
+                .and_then(|win| win.document())
+                .and_then(|doc| {
+                    let dst = doc.get_element_by_id("wasm-body")?;
+                    let canvas = web_sys::Element::from(w.canvas()?);
+                    dst.append_child(&canvas).ok()?;
+                    Some(())
+                })
+                .expect("Couldn't append canvas to document body.");
+
+            let win = web_sys::window().expect("No global `window` exists");
+            let width = win.inner_width().unwrap().as_f64().unwrap() as u32;
+            let height = win.inner_height().unwrap().as_f64().unwrap() as u32;
+
+            let _ = w.request_inner_size(PhysicalSize::new(width.min(2048), height.min(2048)));
+        }
         // The instance is a handle to our GPU
         // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
         let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
@@ -37,7 +88,13 @@ impl<'a> State<'a> {
             ..Default::default()
         });
 
-        let surface = instance.create_surface(window).unwrap();
+        let window_box = Box::pin(w);
+        // SAFETY: `boxed` is pinned, so we can safely create a reference to `window`
+        let window_ref: &'static Window =
+            unsafe { &*(Pin::as_ref(&window_box).get_ref() as *const _) };
+        let size = window_ref.inner_size();
+
+        let surface = instance.create_surface(window_ref).unwrap();
 
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
@@ -47,9 +104,6 @@ impl<'a> State<'a> {
             })
             .await
             .unwrap();
-
-        let adapter_info = adapter.get_info();
-        log::info!("adapter info: {:?}", adapter_info.name);
 
         let (device, queue) = adapter
             .request_device(
@@ -71,6 +125,9 @@ impl<'a> State<'a> {
             .await
             .unwrap();
 
+        let adapter_info = adapter.get_info();
+        log::info!("adapter info: {:?}", adapter_info.name);
+
         let surface_caps = surface.get_capabilities(&adapter);
         // Shader code in this tutorial assumes an Srgb surface texture. Using a different
         // one will result all the colors comming out darker. If you want to support non
@@ -81,32 +138,85 @@ impl<'a> State<'a> {
             .copied()
             .find(|f| f.is_srgb())
             .unwrap_or(surface_caps.formats[0]);
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface_format,
-            width: size.width,
-            height: size.height,
-            present_mode: surface_caps.present_modes[0],
-            alpha_mode: surface_caps.alpha_modes[0],
-            desired_maximum_frame_latency: 2,
-            view_formats: vec![],
-        };
-
-        let clear_color = wgpu::Color::BLACK;
 
         Self {
-            surface,
-            device,
-            queue,
-            config,
-            size,
-            clear_color,
-            window,
+            window: window_box,
+            surface: surface,
+            device: device,
+            queue: queue,
+            config: wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format: surface_format,
+                width: size.width,
+                height: size.height,
+                present_mode: surface_caps.present_modes[0],
+                alpha_mode: surface_caps.alpha_modes[0],
+                desired_maximum_frame_latency: 2,
+                view_formats: vec![],
+            },
+            size: size,
+            clear_color: wgpu::Color::BLACK,
+            surface_configured: false,
         }
     }
 
-    fn window(&self) -> &Window {
-        &self.window
+    #[allow(unused_variables)]
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit()
+            }
+            WindowEvent::Resized(physical_size) => {
+                log::info!("physical_size: {physical_size:?}");
+                self.surface_configured = true;
+                self.resize(physical_size);
+            }
+            WindowEvent::RedrawRequested => {
+                // This tells winit that we want another frame after this one
+                self.window.request_redraw();
+
+                if !self.surface_configured {
+                    return;
+                }
+
+                self.update();
+                match self.render() {
+                    Ok(_) => {}
+                    // Reconfigure the surface if it's lost or outdated
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        self.resize(self.size)
+                    }
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory | wgpu::SurfaceError::Other) => {
+                        log::error!("OutOfMemory");
+                        event_loop.exit();
+                    }
+
+                    // This happens when the a frame takes too long to present
+                    Err(wgpu::SurfaceError::Timeout) => {
+                        log::warn!("Surface timeout")
+                    }
+                }
+            }
+            WindowEvent::CursorMoved {
+                device_id,
+                position,
+            } => {
+                self.clear_color.r = position.x as f64 / self.size.width as f64;
+                self.clear_color.g = position.y as f64 / self.size.height as f64;
+            }
+            _ => {}
+        }
     }
 
     pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
@@ -114,59 +224,45 @@ impl<'a> State<'a> {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
-            self.surface.configure(&self.device, &self.config);
-        }
-    }
-
-    #[allow(unused_variables)]
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::CursorMoved {
-                device_id,
-                position,
-            } => {
-                self.clear_color.r = position.x as f64 / self.size.width as f64;
-                self.clear_color.g = position.y as f64 / self.size.height as f64;
-                true
-            }
-            _ => false,
+            self.surface.configure(&self.device, &self.config)
         }
     }
 
     fn update(&mut self) {}
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
+        if self.surface_configured {
+            let output = self.surface.get_current_texture()?;
+            let view = output
+                .texture
+                .create_view(&wgpu::TextureViewDescriptor::default());
 
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
-            });
+            let mut encoder = self
+                .device
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("Render Encoder"),
+                });
 
-        {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(self.clear_color),
-                        store: wgpu::StoreOp::Store,
-                    },
-                })],
-                depth_stencil_attachment: None,
-                occlusion_query_set: None,
-                timestamp_writes: None,
-            });
+            {
+                let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: Some("Render Pass"),
+                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(self.clear_color),
+                            store: wgpu::StoreOp::Store,
+                        },
+                    })],
+                    depth_stencil_attachment: None,
+                    occlusion_query_set: None,
+                    timestamp_writes: None,
+                });
+            }
+
+            self.queue.submit(iter::once(encoder.finish()));
+            output.present();
         }
-
-        self.queue.submit(iter::once(encoder.finish()));
-        output.present();
-
         Ok(())
     }
 }
@@ -183,96 +279,9 @@ pub async fn run() {
     }
 
     let event_loop = EventLoop::new().unwrap();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
 
-    #[cfg(target_arch = "wasm32")]
-    {
-        // Winit prevents sizing with CSS, so we have to set
-        // the size manually when on web.
-        use winit::dpi::PhysicalSize;
+    let mut app = App::default();
 
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-body")?;
-                let canvas = web_sys::Element::from(window.canvas()?);
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Couldn't append canvas to document body.");
-
-        let win = web_sys::window().expect("No global `window` exists");
-        let width = win.inner_width().unwrap().as_f64().unwrap() as u32;
-        let height = win.inner_height().unwrap().as_f64().unwrap() as u32;
-
-        let _ = window.request_inner_size(PhysicalSize::new(width.min(2048), height.min(2048)));
-    }
-
-    // State::new uses async code, so we're going to wait for it to finish
-    let mut state = State::new(&window).await;
-    let mut surface_configured = false;
-
-    event_loop
-        .run(move |event, control_flow| {
-            match event {
-                Event::WindowEvent {
-                    ref event,
-                    window_id,
-                } if window_id == state.window().id() => {
-                    if !state.input(event) {
-                        // UPDATED!
-                        match event {
-                            WindowEvent::CloseRequested
-                            | WindowEvent::KeyboardInput {
-                                event:
-                                    KeyEvent {
-                                        state: ElementState::Pressed,
-                                        physical_key: PhysicalKey::Code(KeyCode::Escape),
-                                        ..
-                                    },
-                                ..
-                            } => control_flow.exit(),
-                            WindowEvent::Resized(physical_size) => {
-                                log::info!("physical_size: {physical_size:?}");
-                                surface_configured = true;
-                                state.resize(*physical_size);
-                            }
-                            WindowEvent::RedrawRequested => {
-                                // This tells winit that we want another frame after this one
-                                state.window().request_redraw();
-
-                                if !surface_configured {
-                                    return;
-                                }
-
-                                state.update();
-                                match state.render() {
-                                    Ok(_) => {}
-                                    // Reconfigure the surface if it's lost or outdated
-                                    Err(
-                                        wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
-                                    ) => state.resize(state.size),
-                                    // The system is out of memory, we should probably quit
-                                    Err(
-                                        wgpu::SurfaceError::OutOfMemory | wgpu::SurfaceError::Other,
-                                    ) => {
-                                        log::error!("OutOfMemory");
-                                        control_flow.exit();
-                                    }
-
-                                    // This happens when the a frame takes too long to present
-                                    Err(wgpu::SurfaceError::Timeout) => {
-                                        log::warn!("Surface timeout")
-                                    }
-                                }
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-                _ => {}
-            }
-        })
-        .unwrap();
+    event_loop.run_app(&mut app).unwrap();
 }
